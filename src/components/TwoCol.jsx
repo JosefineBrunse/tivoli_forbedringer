9265803e6b01635c10e34b55826cdf9ffa8e9062
“juli81n7” <juli81n7@stud.kea>
2024-05-27 18:21:44 +0200

    Den driler helt sindsygt pt

--- src/components/TwoCol.jsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-import styles from "../styles/grid.css";
-export default function TwoCol({ children, padding }) {
-  return <section className="gridtwocol">{children}</section>;
-}
