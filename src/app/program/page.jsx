8ba1bd2307359050b129eb81cd6b07a0a275971b
“juli81n7” <juli81n7@stud.kea>
2024-05-28 12:08:18 +0200

    Opdateret

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -5,12 +5,12 @@ import Loader from "@/components/Loader";
 import ProgramComponent from "@/components/ProgramComponent";
 import localFont from "next/font/local";
 const myFont = localFont({
-  src: "/../../public/typografi/DomaineDisplayWeb-Black.woff2",
+  src: "/../../../public/typografi/DomaineDisplayWeb-Black.woff2",
 });
 export default function Program() {
   return (
     <main className={"programside"}>
-      <h1 style={myFont.className}>Program</h1>
+      <h1 className={`${myFont.className}`}>Program</h1>
       <ProgramComponent data={programdata} />
     </main>
   );
